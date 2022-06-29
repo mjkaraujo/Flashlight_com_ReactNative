9f9cb31f0ee000f8615d375c872fec00c4aae175
Mauricio Araujo <mjk_araujo@hotmail.com>
2022-06-28 22:42:36 -0300

    flashlight em React Native

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Image, TouchableOpacity } from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
 
 export default function App() {
+  const [toggle, setToggle] = useState(false);
+
+  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
+
+  // Liga o Flash do celular
+  useEffect(() => {
+    Torch.switchState(toggle);
+  }, [toggle]);
+
+  useEffect(() => {
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle);
+    });
+    return () => subscription.remove();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={toggle ? styles.containerLight : styles.container}>
+      <TouchableOpacity onPress={handleChangeToggle}>
+        <Image
+          style={toggle ? styles.lightingOn : styles.lightingOff}
+          source={
+            toggle
+              ? require('./assets/lampadaAcesa.png')
+              : require('./assets/lampadaApagada.png')
+          }
+        />
+        <Image
+          style={toggle ? styles.lightingOn : styles.lightingOff}
+          source={
+            toggle
+              ? require('./assets/logoBranco.png')
+              : require('./assets/logoPreto.png')
+          }
+        />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +47,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightingOn: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+  lightingOff: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
   },
 });
